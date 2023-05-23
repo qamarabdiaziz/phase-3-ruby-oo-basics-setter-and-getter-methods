e35952368a1be39b26f6368507d7487e0654d54b
qamarabdiaziz <tushez98@gmail.com>
2023-05-23 04:19:36 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,45 @@
-class Dog
+# class Dog
+#     def name=(name)
+#         @name = name
+#     end
 
+#     def name
+#         @name
+#     end
+
+#     def breed=(breed)
+#         @breed = breed
+#     end
+
+#     def
+#         @breed
+#     end
+# end
+
+# fido = Dog.new
+# fido.name = "Fido"
+# snoopy = Dog.new
+# snoopy.breed = "Beagle"
+
+class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 end
+
+fido = Dog.new
+fido.name = "Fido"
+snoopy = Dog.new
+snoopy.breed = "Beagle"
